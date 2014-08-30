5b0dfbf05100ffe33c9533cf690427a81c11ff6e
Mike Patella <m.c.patella@gmail.com>
2014-08-30 09:30:06 -0400

    add array of quests to top of game file

--- web/resources/public/js/game.js
+++ web/resources/public/js/game.js
@@ -1,3 +1,49 @@
+var quests = [{
+  title: "Welcome to Quest Quest!",
+  body: "",
+  reward: "Falling Unlocked!"
+}, {
+  title: "Safety First",
+  body, "The ground is fast approaching, you must land safely!",
+  reward, "Move Right Unlocked!"
+}, {
+  title; "Right of Way",
+  body: "Get a feel for your surroundings, press d or right arrow to move right as far as your legs will carry you!",
+  reward: "Move Left Unlocked!"
+}, {
+  title: "Left Alone",
+  body: "Continue to explore your new surroundings, press press a or left arrow to move left!",
+  reward: "Attack Unlocked!"
+}, {
+  title: "A Trial By Combat",
+  body: "Your first enemy blocks the path! You must defeat it to continue. Apply everything you've learned so far to vanquish this beast!",
+  reward: "Leveling Up Has Made You Stronger!"
+}, {
+  title: "Another Trial By Combat",
+  body: "A scarier enemiy blocks your path! Vanquish it to proceed!",
+  reward: "Leveling Up Has Made You Stronger!"
+}, {
+  title: "An Elite Leader",
+  body: "An extremely cunning and ferocious stands between you and the nearby village! Fight with all your power to overcome it!",
+  reward: "Pick-up Item Unlocked!"
+}, {
+  title: "Johnney Applegatherer",
+  body: "Those villagers look very hungry. Gather all of their apples to keep them from starving to death!",
+  reward: "Jump Unlocked!"
+}, {
+  title: "Launch Over It!",
+  body: "Tighten the muscles in your legs to form a spring and launch yourself over the rock.",
+  reward: "Jump-Attack Unlocked!"
+}, {
+  title: "The Epic Raid Boss",
+  body: "Destroy the final boss to see what lies inside the treasure room!",
+  reward: "Open Treasure Unlocked!"
+}, {
+  title: "End Game",
+  body: "Congratulations, you've reached the peak of your power! Surely untold adventures lie before you.",
+  reward: "End-Game Content Unlocked!"
+}];
+
 // Physics constants
 var MAX_SPEED = 500;     // Pixels / second
 var JUMP_SPEED = -250;   // Pixels / second (negative y is u p )
@@ -50,12 +96,11 @@ GameState.prototype.create = function() {
   this.map.addTilesetImage('tile-set', 'tileSet');
   this.map.addTilesetImage('tile-map', 'tileMap');
 
-  var background = this.map.createLayer('background');
-  background.resizeWorld();
   // Add layers to world, then resize both to match world.
   var walls = this.map.createLayer('walls');
   walls.resizeWorld();
-  this.walls = walls;
+
+  this.map.setCollisionBetween(0, 15);
 
   // Create player ingame
   var spawn = {
@@ -96,6 +141,7 @@ GameState.prototype.create = function() {
 
 GameState.prototype.update = function() {
   this.game.physics.arcade.collide(this.player, this.walls);
+
   if (!gameIsPaused) {
 
     // Handle input
